bc688b253a003cbc1ef7bb1eb60026825bf9a497
bogdan <bogdan4888@gmail.com>
2025-07-29 22:28:09 +0300

    spring cloud bus, rabbitmq

--- accounts/src/main/java/com/bogdan/accounts/dto/AccountsContactInfoDTO.java
+++ accounts/src/main/java/com/bogdan/accounts/dto/AccountsContactInfoDTO.java
@@ -6,8 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDTO(String message,
-                                     Map<String, String> contactDetails,
-                                     List<String> onCallSupport) {
+public class AccountsContactInfoDTO {
 
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
+
+    //Getter, Setter, ToString
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
 }
--- cards/src/main/java/com/bogdan/cards/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/bogdan/cards/dto/CardsContactInfoDTO.java
@@ -6,7 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDTO(String message,
-                                  Map<String, String> contactDetails,
-                                  List<String> onCallSupport) {
+public class CardsContactInfoDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
+
+    //Getter, Setter, ToString
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
 }
