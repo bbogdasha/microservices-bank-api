a51cf0d3b85d41080b7bfc46172fb9e7ff05b77f
bogdan <bogdan4888@gmail.com>
2025-09-17 22:27:42 +0300

    Added and updated gatewayservice

---
+++ gatewayserver/src/main/java/com/bogdan/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,40 @@
+package com.bogdan.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator msBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/msbank/accounts/**")
+						.filters( f -> f.rewritePath("/msbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/msbank/loans/**")
+						.filters( f -> f.rewritePath("/msbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/msbank/cards/**")
+						.filters( f -> f.rewritePath("/msbank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+
+
+	}
+
+}
